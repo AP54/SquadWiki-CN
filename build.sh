866551f480cd9aced6be7f726654c51d26358508
AP54 <321918223@qq.com>
2023-03-12 16:22:16 +0800

    fix build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,7 @@
-mkdocs build;chmod 777 ./scripts/SitemapConverter/main.py;chmod 777 ./scripts/PagelinkConverter/main.py; ./scripts/SitemapConverter/main.py; ./scripts/PagelinkConverter/main.py; chmod 777 ./urls.txt; curl -H 'Content-Type:text/plain' --data-binary @urls.txt BAIDU_API;
+mkdocs build;
+chmod 777 ./scripts/SitemapConverter/main.py;
+chmod 777 ./scripts/PagelinkConverter/main.py;
+python ./scripts/SitemapConverter/main.py;
+python ./scripts/PagelinkConverter/main.py;
+chmod 777 ./urls.txt;
+curl -H 'Content-Type:text/plain' --data-binary @urls.txt "$BAIDU_API";
